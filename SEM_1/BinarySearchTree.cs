d1d1692d3f07d4ad4622a085b81b4fe95cfe5b63
dRevan1 <filippokorny699@gmail.com>
2025-10-14 14:57:48 +0200

    Hotový základ BST

--- SEM_1/BinarySearchTree.cs
+++ SEM_1/BinarySearchTree.cs
@@ -8,12 +8,71 @@ namespace SEM_1
 {
    public class BinarySearchTree<T> where T : IComparable<T>
     {
-       public BinarySearchTreeNode<T> Root { get; set; }
+       public BinarySearchTreeNode<T>? Root { get; set; }
 
-        public void Find(T data)
+        private void DeleteNode(BinarySearchTreeNode<T> node)
         {
-            BinarySearchTreeNode<T> current = Root;
-            while (current != null) {
+            BinarySearchTreeNode<T>? parent = node.Parent;
+            if (node.LeftChild == null && node.RightChild == null) // bez potomkov/synov
+            {
+                if (parent == null) // ak je koreň
+                {
+                    Root = null;
+                    return;
+                }
+                if (parent.LeftChild == node)
+                {
+                    parent.LeftChild = null;
+                }
+                else
+                {
+                    parent.RightChild = null;
+                }
+            }
+            else if (node.LeftChild != null && node.RightChild != null) // má oboch synov
+            {
+                BinarySearchTreeNode<T> successor = FindSuccessor(node)!;
+                T deleteData = node.Data;
+                node.Data = successor.Data;
+                successor.Data = deleteData;
+                DeleteNode(successor);
+            }
+            else
+            {
+                BinarySearchTreeNode<T> childToPromote = (node.LeftChild != null) ? node.LeftChild! : node.RightChild!;
+
+                if (parent == null) // ak je koreň
+                {
+                    Root = childToPromote;
+                    Root.Parent = null;
+                    return;
+                }
+
+
+                if (parent.LeftChild == node)
+                {
+                    parent.LeftChild = childToPromote;
+                }
+                else
+                {
+                    parent.RightChild = childToPromote;
+                }
+                childToPromote.Parent = parent;
+            }
+        }
+
+        private BinarySearchTreeNode<T>? TryToFindNode(T data)
+        {
+            if (Root == null)
+            {
+                return null;
+            }
+            BinarySearchTreeNode<T>? current = Root;
+            BinarySearchTreeNode<T> parent = Root;
+
+            while (current != null)
+            {
+                parent = current;
                 if (current.Data.CompareTo(data) < 0)
                 {
                     current = current.RightChild;
@@ -24,53 +83,72 @@ namespace SEM_1
                 }
                 else
                 {
-                    break;
+                    return current;
                 }
             }
+            return parent;
+        }
 
-           Console.WriteLine(current != null ? ("Found: " + current.Data) : "Not Found");
+        private BinarySearchTreeNode<T>? FindSuccessor(BinarySearchTreeNode<T> node)
+        {
+            if (node.RightChild == null)
+            {
+                return null;
+            }
+            BinarySearchTreeNode<T> current = node.RightChild!;
+            while (current.LeftChild != null)
+            {
+                current = current.LeftChild;
+            }
+            return current;
+        }
+
+        public BinarySearchTreeNode<T>? Find(T data)
+        {
+            BinarySearchTreeNode<T>? node = TryToFindNode(data);
+            if (node != null && node.Data.CompareTo(data) == 0)
+            {
+                return node;
+            }
+
+            Console.WriteLine("Value not found in the tree.");
+            return null;
         }
 
         public void Insert(T data)
         {
-            BinarySearchTreeNode<T> current = Root;
-            if (Root == null)
+            BinarySearchTreeNode<T>? parent = TryToFindNode(data);
+            if (parent == null) // strom je prázdny - uložíme do koreňa
             {
                 Root = new BinarySearchTreeNode<T> { Data = data };
                 return;
             }
-            while (current != null)
+            if (parent.Data.CompareTo(data) == 0)
             {
-                if (current.Data.CompareTo(data) < 0)
-                {
-                    if (current.RightChild == null)
-                    {
-                        current.RightChild = new BinarySearchTreeNode<T> { Data = data, Parent = current };
-                        return;
-                    }
-                    else
-                    {
-                        current = current.RightChild;
-                    }
-                }
-                else if (current.Data.CompareTo(data) > 0)
-                {
-                    if (current.LeftChild == null)
-                    {
-                        current.LeftChild = new BinarySearchTreeNode<T> { Data = data, Parent = current };
-                        return;
-                    }
-                    else
-                    {
-                        current = current.LeftChild;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Value is already in the tree.");
-                    break;
-                }
+                Console.WriteLine("Value is already in the tree.");
+                return;
             }
+
+            BinarySearchTreeNode<T> nodeToInsert = new BinarySearchTreeNode<T> { Data = data, Parent = parent };
+            if (parent.Data.CompareTo(data) < 0)
+            {
+                parent.RightChild = nodeToInsert;
+            }
+            else
+            {
+                parent.LeftChild = nodeToInsert;
+            }
+        }
+
+        public void Delete(T data)
+        {
+            BinarySearchTreeNode<T>? nodeToDelete = Find(data);
+            if (nodeToDelete == null)
+            {
+                Console.WriteLine("Value not found in the tree.");
+                return;
+            }
+            DeleteNode(nodeToDelete);
         }
     }
 }
--- SEM_1/BinarySearchTreeNode.cs
+++ SEM_1/BinarySearchTreeNode.cs
@@ -9,9 +9,9 @@ namespace SEM_1
     public class BinarySearchTreeNode<T> where T : IComparable<T>
     {
         public T Data { get; set; }
-        public BinarySearchTreeNode<T> Parent { get; set; }
-        public BinarySearchTreeNode<T> LeftChild { get; set; }
-        public BinarySearchTreeNode<T> RightChild { get; set; }
+        public BinarySearchTreeNode<T>? Parent { get; set; }
+        public BinarySearchTreeNode<T>? LeftChild { get; set; }
+        public BinarySearchTreeNode<T>? RightChild { get; set; }
 
     }
 }
